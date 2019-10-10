e66685f03c8fd26fb6d90a7f3bea971ad8b6b4d0
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-10-10 23:07:06 +0700

    Add files via upload

---
+++ IF1-10118040-Latihan 18-Kelipatan/src/if1/pkg10118040/latihan/pkg18/kelipatan/IF110118040Latihan18Kelipatan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118040.latihan.pkg18.kelipatan;
+
+/**
+ *
+ * @author asus
+* NAMA			: Moch Yudha Rusdian
+ * KELAS		: IF-1
+ * NIM			: 10118040
+ * Deskripsi Program	: Kelipatan
+*/
+public class IF110118040Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    double a = 3.5, b = 0;
+		for (int i = 1; i <= 10; i++, b = a * i)
+			System.out.println(b);
+
+	
+    }
+    
+}
